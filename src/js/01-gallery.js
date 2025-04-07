a3d8fbfdc22fcad865e07c280379a79692003947
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-04-07 17:49:34 +0300

    code does not works

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,8 +1,53 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
 // Change code below this line
 
 console.log(galleryItems);
 
 
+const container = document.querySelector('.gallery');
+const markup = galleryItems.map(
+  item => `
+   <li class="gallery__item js-target">
+     <a class="gallery__link js-target" href="${item.original}">
+       <img class="gallery__image js-target" src="${item.preview}" 
+        data-source="${item.original}"
+        alt="${item.description}">
+     </a>
+    </li>
+`
+);
 
+container.insertAdjacentHTML('beforeend', markup.join(''));
+container.addEventListener('click', onclick);
+
+function onclick(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('js-target')) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `
+   <img src="${evt.target.dataset.source}">;
+`,
+    {
+      onShow: instance => {
+        window.addEventListener('keydown', onEscapeCloseModal);
+      },
+      onClose: instance => {
+        window.removeEventListener('keydown', onEscapeCloseModal);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscapeCloseModal(event) {
+    if (event.key === 'Escape') {
+      //  console.log("Escape works");
+      instance.close();
+    }
+  }
+}
