4d48ba312e303fc3258d883dcf4bc2265de7e879
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-04-11 19:06:22 +0300

    localStorage does not works

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
-console.log('hi');
+import Player from '@vimeo/player';
+console.log('hi 2');
+const playerEl = document.querySelector('iframe');
+const player = new Player(playerEl);
+
+player.on('timeupdate', onPlay);
+
+function onPlay(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+
+// player.on('play', function () {
+//   console.log('Video is playing!');
+// });
