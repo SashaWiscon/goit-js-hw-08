9b87655699929d6fb3acf90b4452e9d0830453b4
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-04-14 23:16:42 +0300

    task3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,32 @@
 import Player from '@vimeo/player';
-console.log('hi 2');
+import throttle from 'lodash.throttle';
+
 const playerEl = document.querySelector('iframe');
 const player = new Player(playerEl);
 
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000));
 
 function onPlay(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+player
+  .setCurrentTime(currentTime)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 
 // player.on('play', function () {
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,2 +1,9 @@
 
-console.log('hi 3');
+
+const formEl = document.querySelector('.feedback-form');
+formEl.addEventListener('input', onFormInput);
+
+function onFormInput(evt) {
+    evt.preventDefault();
+    console.log(evt.target.elements.name.value);
+}
