011283dbde2cabda92d19e438237ca0789f6d01f
SashaWiscon <108764697+SashaWiscon@users.noreply.github.com>
2025-04-17 20:41:36 +0000

    Deploying to gh-pages from @ SashaWiscon/goit-js-hw-08@39fce66935c3344c843f5a9d6011c3d9aee1224c 🚀

--- 03-feedback.2a2f1e87.js
+++
@@ -1,2 +0,0 @@
-console.log("Скрипт загружен"),document.querySelector(".feedback-form").addEventListener("input",(function(e){e.preventDefault(),console.log(e.target.value)}));
-//# sourceMappingURL=03-feedback.2a2f1e87.js.map
